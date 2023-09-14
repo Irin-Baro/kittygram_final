6a4724bb1a5c5e01d1f88d8e23be37efc23fdd21
Ирина Баронская <barons42@yandex.ru>
2023-09-14 21:39:25 +0300

    Fix ALLOWED_HOSTS

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['51.250.108.182', '127.0.0.1', 'localhost', 'baro-kittygram.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
