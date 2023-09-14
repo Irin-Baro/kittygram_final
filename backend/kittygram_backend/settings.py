2f4c12d0c69e411eb045ea4a780099cd09a635c9
Ирина Баронская <barons42@yandex.ru>
2023-09-14 21:06:53 +0300

    Fix settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,12 +59,12 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME': os.getenv('POSTGRES_DB', 'django'),
-      'USER': os.getenv('POSTGRES_USER', 'django'),
-      'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-      'HOST': os.getenv('DB_HOST', 'db'),
-      'PORT': os.getenv('DB_PORT', 5432)
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
